6a5fb26c69670d1ce55c5b68824c9d96397e1354
Abdusalomov Azam <97389595+ritnyxdev@users.noreply.github.com>
2023-01-07 21:58:34 +0500

    Delete tempCodeRunnerFile.js

--- tempCodeRunnerFile.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require("bcryptjs");
-
-console.log(
-    bcrypt.compareSync(
-        "xUDehxQO",
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNjBiNTFlYjhkMzVhZGZhO…"
-    )
-);
